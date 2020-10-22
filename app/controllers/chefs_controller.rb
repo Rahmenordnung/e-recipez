2d0663dbf7dfc98c37bce50a45570b13add68455
Tudor Pascu <tudorpascu787@gmail.com>
2020-10-22 22:05:00 +0000

    add edit functionality after creating tests

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,28 +1,41 @@
 class ChefsController < ApplicationController
-    
-    def new
-        @chef = Chef.new
+  
+  def new
+    @chef = Chef.new
+  end
+  
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
+      redirect_to chef_path(@chef)
+    else
+      render 'new'
     end
-    
-    def create
-        @chef = Chef.new(chef_params)
-        if @chef.save
-          flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
-          redirect_to chef_path(@chef)
-        else
-          render 'new'
-        end
-    end
-    
-    def show
-        @chef = Chef.find(params[:id])
-    end
-        
-    private
-    
-    def chef_params
-        params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
-    end
-    
+  end
+  
+  def show
+    @chef = Chef.find(params[:id])
+  end
+  
+  def edit
+    @chef = Chef.find(params[:id])
+  end
 
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to @chef
+    else
+      render 'edit'
+    end  
+  end
+  
+  private
+  
+  def chef_params
+    params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
+  end
+  
 end
